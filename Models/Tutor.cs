1dbd7c8e4d5e3f781f62de27da41fcf7c70e9a48
javierplo12 <javierplo841@gmail.com>
2025-02-06 13:35:52 +0100

    Creado Model de tutor

---
+++ Models/Tutor.cs
@@ -0,0 +1,23 @@
+namespace Models
+{
+    public class Tutor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string DNI { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Activo { get; set; }
+        public string Rol { get; } = "TUTOR";
+        public Tutor(string nombre, string dni, string email, string username, string password, bool activo = true)
+        {
+            Nombre = nombre;
+            DNI = dni;
+            Email = email;
+            Username = username;
+            Password = password;
+            Activo = activo;
+        }
+    }
+}
