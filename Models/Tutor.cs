875271c16198738e5b5cea55f7c333f941e4e893
a25612 <a25612@svalero.com>
2025-02-20 13:22:08 +0100

    actualizamos model de Tutores para EFcore

--- Models/Tutor.cs
+++ Models/Tutor.cs
@@ -1,15 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Models
 {
     public class Tutor
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(9)]
         public string DNI { get; set; }
+
+        [MaxLength(255)]
         public string Email { get; set; }
+
+        [MaxLength(255)]
         public string Username { get; set; }
+
+        [MaxLength(255)]
         public string Password { get; set; }
-        public bool Activo { get; set; }
+
+        [Required]
+        public bool Activo { get; set; } = true;
+
         public string Rol { get; } = "TUTOR";
+
+        // Relación con Usuarios_Tutores (N:N)
+        public ICollection<UsuarioTutor> UsuariosTutores { get; set; }
+
+        // Constructor vacío (necesario para EF Core)
+        public Tutor() {}
+
+        // Constructor completo
         public Tutor(int id, string nombre, string dni, string email, string username, string password, bool activo = true)
         {
             Id = id;
@@ -19,6 +45,7 @@ namespace Models
             Username = username;
             Password = password;
             Activo = activo;
+            UsuariosTutores = new List<UsuarioTutor>();
         }
     }
 }
