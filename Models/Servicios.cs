447ffe0b27d310c79bd00ecb7f3024ef95eac076
a25612 <a25612@svalero.com>
2025-02-20 13:21:23 +0100

    Actualizamos el Model de Servicios para EfCore

--- Models/Servicios.cs
+++ Models/Servicios.cs
@@ -1,16 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Models
 {
-
     public class Servicio
-{
-    public int Id { get; set; }
-    public string Nombre { get; set; }
-    public decimal Precio { get; set; }
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Nombre { get; set; }
+
+        [Required]
+        public decimal Precio { get; set; }
+
+        // Relación con Centros (N:N)
+        public ICollection<ServicioCentro> ServiciosCentros { get; set; }
+
+        // Relación con Sesiones
+        public ICollection<Sesiones> Sesiones { get; set; }
+
+        // Constructor vacío (necesario para EF Core)
+        public Servicio() {}
+
+        // Constructor completo
         public Servicio(int id, string nombre, decimal precio)
         {
             Id = id;
             Nombre = nombre;
             Precio = precio;
+            ServiciosCentros = new List<ServicioCentro>();
+            Sesiones = new List<Sesiones>();
         }
     }
 }
