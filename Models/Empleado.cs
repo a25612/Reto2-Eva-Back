8c733de9960c4d9f3d03625befbb2e58ad3b236a
a25612 <a25612@svalero.com>
2025-02-20 13:20:13 +0100

    Actualizamos el model para usar EFCore

--- Models/Empleado.cs
+++ Models/Empleado.cs
@@ -1,16 +1,45 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Models
 {
     public class Empleado
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(9)]
         public string DNI { get; set; }
+
         public int JornadaTotalHoras { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Username { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Password { get; set; }
+
         public string Rol { get; } = "EMPLEADO";
 
-        public Empleado(int id, string nombre, string dni, int jornadaTotalHoras, string username, string password)
+        [ForeignKey("Centro")]
+        public int IdCentro { get; set; }
+        public Centros Centro { get; set; }
+
+        // Relación con Sesiones
+        public ICollection<Sesiones> Sesiones { get; set; }
+
+        // Constructor vacío (necesario para EF Core)
+        public Empleado() {}
+
+        // Constructor completo
+        public Empleado(int id, string nombre, string dni, int jornadaTotalHoras, string username, string password, int idCentro)
         {
             Id = id;
             Nombre = nombre;
@@ -18,6 +47,7 @@ namespace Models
             JornadaTotalHoras = jornadaTotalHoras;
             Username = username;
             Password = password;
+            IdCentro = idCentro;
         }
     }
 }
