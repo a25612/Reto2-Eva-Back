20d3ad7edf983efea8e82562b1b6620a44e3c0b8
a25612 <a25612@svalero.com>
2025-02-24 14:01:20 +0100

    añadimos json ignore en servicio

--- Models/Servicio.cs
+++ Models/Servicio.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Models
 {
@@ -14,6 +15,7 @@ namespace Models
         [Required]
         public decimal Precio { get; set; }
 
+        [JsonIgnore] 
         public ICollection<ServicioCentro> ServiciosCentros { get; set; }
 
         public ICollection<Sesion> Sesiones { get; set; }
